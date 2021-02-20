5e612fb8263c231776127fc9ac4a15617b78012d
Davi Rolim <davirolim94@gmail.com>
2021-02-20 15:56:19 -0300

    start pipeline

--- pipelines_webinar/webservice_stage.py
+++ pipelines_webinar/webservice_stage.py
@@ -5,7 +5,7 @@ from .pipelines_webinar_stack import PipelinesWebinarStack
 class WebServiceStage(core.Stage):
   def __init__(self, scope: core.Construct, id: str, **kwargs):
     super().__init__(scope, id, **kwargs)
-
+    # core.Stage
     service = PipelinesWebinarStack(self, 'WebService')
 
     self.url_output = service.url_output
