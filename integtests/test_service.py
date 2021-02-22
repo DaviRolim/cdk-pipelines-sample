4686fe8df938a4d52959b091e289efd01e4b7f01
Davi Rolim <davirolim94@gmail.com>
2021-02-22 12:14:07 -0300

    changing the test

--- integtests/test_service.py
+++ integtests/test_service.py
@@ -11,7 +11,7 @@ def test_200_response():
 
 def test_cretea_user():
   url = os.environ['SERVICE_URL'] + '/users'
-  payload = {'{"user":{"firstName":"Davi","lastName":"Holanda","email":"davirolim94@gmail.com"}}'}
+  payload = {"user":{"firstName":"Davi","lastName":"Holanda","email":"davirolim94@gmail.com"}}
   with requests.post(url, payload) as response:
     print(response)
     assert response.status_code == 200
